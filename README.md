b96d59f04e21d6683c59c57ecb9631aeb55dc3ec
Travis Clinkscales <clink73@gmail.com>
2019-02-10 19:39:23 -0600

    Version 2.0 of my Background Image Retriever

--- README.md
+++ README.md
@@ -4,9 +4,15 @@
 
 ### This is just a very simple script that I made in like 10 minutes. I was looking at the background images that I get on Windows 10 and I'm always wishing I could find images like that. I did a quick google on where these images are located and come to find out that the images are pulled from the internet and saved on my machine. So I decided to get those images before they are deleted by Windows by setting up a task and scheduled it to run every morning at 6 AM to save to a folder in my Pictures. It works like a charm and is pretty simple.
 
+## Installation/Use
+
+### You should be able to run this right off the bat. Get the executable from the dist folder and everything **__should__** go smoothly.
+
 ## Improvements
 
-### One improvement that I would like to make that I have no clue how to do is that the images are mixed between Desktop and Mobile. Anything over 150KB is an image that I want to save in my folder. I don't know how to ignore the Mobile images and keep the Desktop images. So I still have a manual piece of going in and moving the mobile pictures to another folder by having to look at each individual picture and seeing that it has the dimensions for a mobile phone, but I thought what I have is good enough. So I hope you enjoy!
+### ~~One improvement that I would like to make that I have no clue how to do is that the images are mixed between Desktop and Mobile. Anything over 150KB is an image that I want to save in my folder. I don't know how to ignore the Mobile images and keep the Desktop images. So I still have a manual piece of going in and moving the mobile pictures to another folder by having to look at each individual picture and seeing that it has the dimensions for a mobile phone, but I thought what I have is good enough.~~ I have made the improvement now to move mobile images to the mobile folder and keep the desktop images in the Test folder. I used OpenCV to do this and was very simple to do surprisingly. I'm getting the dimensions of the picture to decide if it should move to the mobile folder or stay in the Test folder. I am also using OpenCV to decide if I already have the image stored in my Test folder. This is likely since my Task Scheduler is set to run everyday. 
+
+### My next improvement will be to add Mapping/Hashing. As of right now I take the image from the Assets folder => go through each image in the Test folder to see if their is a duplicate => if there is then delete it else keep it. So this takes a long time. I want to set up Mapping/Hashing that way we just do one check for a matching hash and that's it. I don't know anything about this so yet another opportunity to learn. I'll be happy to hear any other suggestions or improvements as well!
 
 ## Why?
 
--- getspotlightimages.py
+++ getspotlightimages.py
@@ -1,32 +1,134 @@
 import os
-import shutil
-import datetime
+from shutil import copy, move
+from datetime import datetime
+from cv2 import imread, split, subtract, countNonZero
 
-pathToImages = os.path.join("C:\\", "Users", "<USERNAME>", "AppData", "Local", "Packages", 
-                      "Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy", "LocalState", "Assets")
-pathToTest = os.path.join("C:\\", "Users", "<USERNAME>", "Pictures")
-filesWithSizes = []
-filteredFiles = []
+# load home path
+PC_NAME = os.environ['HOME']
 
-os.chdir(pathToImages)
-files = os.listdir(".")
+pathToImages = os.path.join("C:\\", "Users", PC_NAME, "AppData", "Local", "Packages", "Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy", "LocalState", "Assets")
+pathToTest =   os.path.join("C:\\", "Users", PC_NAME, "Pictures", "Test")
+pathToMobile = os.path.join(pathToTest, "Mobile")
 
-for file in files: 
-    locationOfFile = os.path.join(pathToImages, file)
-    sizeOfFile = os.path.getsize(file)
-    filesWithSizes.append((sizeOfFile, file))
+currentYear = str(datetime.now().year)
 
-for file in filesWithSizes:
-    if(file[0] > 150000):
-        filteredFiles.append(file[1])
+'''
+    Function to decide if the incoming image from the Assets folder
+    is an image that is already saved in the given folder.
+
+    Parameters: 
+        isDesktopImage: boolean - tells whether we need to change to Mobile folder or stay in the Test folder
+        renamedFile: string - name of the currect file/image that we retrieved from the Assets folder
+        incomingImage: bitmap - cv2 object to compare shapes and rgb values of given image
+'''
+def checkForDuplicates(isDesktopImage, renamedFile, incomingImage):
+    # used to tell if we need to rename the image once decided 
+    # that it will be kept in either the Test or Mobile folder
+    isDeleted = False
+
+    # we need to change the directory if to the
+    # Mobile folder if it is a mobile image
+    if not isDesktopImage:
+        os.chdir(pathToMobile)
+
+    # list the files in the current directory
+    imagesInTest = os.listdir(".")
+    for image in imagesInTest: 
         
-filteredFiles.sort(key=lambda s: s[0])
-filteredFiles.reverse()
+        # skip directories
+        if os.path.isdir(image):
+            continue
+        
+        # read the current image in the test folder
+        matchingImage = imread(image)
+        
+        ''' TODO/HACK: Why "if currentYear + "-" not in image"?
+                Basically I'm saving the image in the Test folder first and then checking for duplicates
+                on the incoming image from the Assets folder and there will always be a duplicate because 
+                I'm going through every image in the Test folder which includes the image that I just saved
+                from the Assets folder. Need to find a better way to do this but it will do for now.
+        '''
+        # if the incoming image from the Assets folder and the image in my 
+        # Test folder match then we have possible duplicate images
+        if currentYear + "-" not in image and incomingImage.shape == matchingImage.shape:
+            # get the difference in the images
+            difference = subtract(incomingImage, matchingImage)
 
-for file in filteredFiles:
-    print("File path: ", os.path.abspath(file))
-    shutil.copy(os.path.abspath(file), pathToTest)
-    os.chdir(pathToTest)
-    os.rename(file, str(datetime.datetime.now().isoformat().replace(":", "-")) + ".jpg")
+            # extract the r, g, b values from the difference
+            r, g, b = split(difference)
+
+            # if their are no pixels in the image, that means the images were exactly identical
+            if countNonZero(r) == 0 and countNonZero(g) == 0 and countNonZero(b) == 0:
+                isDeleted = True
+                # removing image from Test folder
+                os.remove(renamedFile)
+                break
+
+    # if it is not deleted
+    if not isDeleted: 
+        # we need to remove the 2019- in the name of the image so that 
+        # when we check for duplicates, we always check for every image
+        os.rename(renamedFile, renamedFile[5:])
+    return
+
+'''
+    Main function to start the process which goes to the Assets folder,
+    copies the image over to the Test folder, renames the image, decide
+    if image is for Mobile or Desktop and moves accordingly, checks for
+    duplicates and either deletes the image or keeps the image.
+'''
+def main():
+
+    # if the Test folder or Mobile folder does not already exist in the Pictures folder
+    if not os.path.isdir(pathToTest) or not os.path.isdir(pathToMobile):
+        # make the Test folder and the Mobile folder
+        os.makedirs(pathToMobile, exist_ok=True)
+
+    # change directories to the original images
     os.chdir(pathToImages)
 
+    # read all the files in the directory
+    files = os.listdir(".")
+    for file in files: 
+        # get the size of the current file
+        sizeOfFile = os.path.getsize(file)
+
+        # if the size is less than 150KB then skip and continue to next image
+        if(sizeOfFile < 150000):
+            continue
+
+        # copy the file over to my test folder
+        copy(os.path.abspath(file), pathToTest)
+
+        # change the directory to manipulate the copied file
+        os.chdir(pathToTest)
+
+        # set the filename to a unique name
+        renamedFile = str(datetime.now().isoformat().replace(":", "-")) + ".jpg"
+
+        # rename the file
+        os.rename(file, renamedFile)
+
+        # read the incoming image 
+        incomingImage = imread(renamedFile)
+
+        # extract the 3 variables that come from shape
+        height, width, channels = incomingImage.shape
+        
+        # if the image is mobile
+        if height == 1920 and width == 1080 and channels == 3:
+            # put it in the mobile folder
+            move(renamedFile, os.path.join(pathToMobile))
+            checkForDuplicates(False, renamedFile, incomingImage) 
+        else:
+            checkForDuplicates(True, renamedFile, incomingImage)
+
+        # else it is a desktop image so leave it in the Test folder
+        # and change back to the directory with the original images
+        os.chdir(pathToImages)
+
+'''
+    Beginning of script
+'''
+if __name__ == "__main__":
+    main()
